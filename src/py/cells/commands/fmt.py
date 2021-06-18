0ea86888151535bb1ae71edbcdeb08ad4b53b7bd
Sébastien Pierre <sebastien.pierre@gmail.com>
2021-06-18 18:12:01 +1200

    [Update] fmt: added -o option

--- src/py/cells/commands/fmt.py
+++ src/py/cells/commands/fmt.py
@@ -16,18 +16,25 @@ class FMT(Command):
         super().define(parser)
         parser.add_argument("-t", "--to", dest="format", action="store", default="cells",
                             help=f"Output format: {', '.join(self.FORMATS)}")
+        parser.add_argument("-o", "--output", dest="output", action="store", default="-",
+                            help=f"Output file")
         parser.add_argument("files", metavar="FILE", type=str, nargs='*',
                             help='Input files to format')
 
     def run(self, args):
         doc = parse(*(Path(_) for _ in args.files))
-        out = sys.stdout
+        if args.output == "-":
+            out = sys.stdout
+        else:
+            out = open(args.output, "w")
         if args.format == "md":
             for chunk in doc.iterMarkdown():
                 out.write(chunk)
         else:
             for chunk in doc.iterSource():
                 out.write(chunk)
+        if out != sys.stdout:
+            out.close()
         # sys.stdout.write(doc.toSource())
         # sys.stdout.write(doc.toSource())
 
